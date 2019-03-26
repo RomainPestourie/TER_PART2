2958c21ad820be5d0623b6bc6f0a6493bf2fd7d6
PESTOURIE Romain <romain.pestourie24@gmail.com>
2019-03-26 18:24:45 +0100

    On y est presque

--- mat.cpp
+++ mat.cpp
@@ -17,21 +17,21 @@ using namespace Eigen;
 using namespace std;
 
 Matrices::Matrices( const double x_min, const double x_max, const double y_min, const double y_max):
-_Nx(Nx),_Ny(Ny),_x_min(x_min), _x_max(x_max), _y_min(y_min), _y_max(y_max)
+_Nx(Nl),_Ny(Nc),_x_min(x_min), _x_max(x_max), _y_min(y_min), _y_max(y_max)
 {
   // Paramètres
-  _M.resize(_Nx*_Ny,_Nx*_Ny);
+  _M.resize(_Nl*_Nc,_Nl*_Nc);
   _Lm = 10;
   _RhoV = 1500;
   _RhoP = 2;
-  _Rho.resize(_Nx,_Ny);
-  _RhoStar.resize(_Nx,_Ny);
-  _Xi.resize(_Nx,_Ny);
-  _XiStar.resize(_Nx,_Ny);
-  _Lambda.resize(_Nx,_Ny);
-  _A.resize(_Nx,_Ny);
-  _AStar.resize(_Nx,_Ny);
-  _T.resize(_Nx,_Ny);
+  _Rho.resize(_Nl,_Nc);
+  _RhoStar.resize(_Nl,_Nc);
+  _Xi.resize(_Nl,_Nc);
+  _XiStar.resize(_Nl,_Nc);
+  _Lambda.resize(_Nl,_Nc);
+  _A.resize(_Nl,_Nc);
+  _AStar.resize(_Nl,_Nc);
+  _T.resize(_Nl,_Nc);
   _LambV = 3;
   _LambP = 4;
   _Cpp = 1000;
@@ -39,13 +39,14 @@ _Nx(Nx),_Ny(Ny),_x_min(x_min), _x_max(x_max), _y_min(y_min), _y_max(y_max)
   _T0 = 293;
   _dt = 20 ;
   _t = 0;
-  _L1.resize(_Nx,_Ny); _L2.resize(_Nx,_Ny); _L3.resize(_Nx,_Ny); _L4.resize(_Nx,_Ny);
-  _P1.resize(_Nx,_Nx); _P2.resize(_Nx,_Nx); _P3.resize(_Nx,_Nx); _P4.resize(_Nx,_Nx);
+  _L1.resize(_Nl,_Nc); _L2.resize(_Nl,_Nc); _L3.resize(_Nl,_Nc); _L4.resize(_Nl,_Nc);
+  //_P1.resize(_Nc,_Nc); _P2.resize(_Nc,_Nc); _P3.resize(_Nc,_Nc); _P4.resize(_Nc,_Nc);
+  _P.resize(_Nc*_Nl,_Nc*_Nl);
 
-  for (int i=0 ; i<_Nx; i++)
+  for (int i=0 ; i<_Nl; i++)
   {
     cout << "i = " << i << endl;
-    for (int j=0 ; j<_Ny ; j++)
+    for (int j=0 ; j<_Nc ; j++)
     {
       cout << "j = " << j << endl;
       _T(i,j) = _T0;
@@ -60,9 +61,9 @@ _Nx(Nx),_Ny(Ny),_x_min(x_min), _x_max(x_max), _y_min(y_min), _y_max(y_max)
 
 void Matrices::Xi()
 {
-  for (int i=0; i<_Nx; i++)
+  for (int i=0; i<_Nl; i++)
   {
-    for (int j=0 ; j<_Ny; j++)
+    for (int j=0 ; j<_Nc; j++)
     {
       _Xi(i,j) = (_RhoV - _Rho(i,j)) / (_RhoV-_RhoP);
     }
@@ -71,9 +72,9 @@ void Matrices::Xi()
 
 void Matrices::Lambda()
 {
-  for (int i=0; i<_Nx; i++)
+  for (int i=0; i<_Nl; i++)
   {
-    for (int j=0 ; j<_Ny ; j++)
+    for (int j=0 ; j<_Nc ; j++)
     {
       _Lambda(i,j) = (1- _Xi(i,j)) * _LambV + _Xi(i,j) * _LambP ;
     }
@@ -82,10 +83,11 @@ void Matrices::Lambda()
 
 void Matrices::A()
 {
-  for (int i=0 ; i<_Nx; i++)
+  for (int i=0 ; i<_Nl; i++)
   {
-    for (int j=0 ; j<_Ny ; j++)
-    {
+    for (int j=0 ; j<_Nc ; j++)
+    {triplets.push_back({0,0,_R(0,0)-(L1(0,0)+_L2(0,0)+L3(0,0)+_L4(0,0))});
+  triplets.push_back({1,0,_L2(0,1)});
       _A(i,j) = (1 - _Xi(i,j)) * _Cpv * _RhoV + _Xi(i,j) * _Cpp * _RhoP;
     }
   }
@@ -94,17 +96,17 @@ void Matrices::A()
 void Matrices::L1234()
 {
   _L1.setZero();_L2.setZero();_L3.setZero();_L4.setZero();
-  for (int i=0; i<_Ny; i++)
+  for (int i=0; i<_Nl; i++)
   {
-    for (int j=1 ; j<_Nx; j++)
+    for (int j=1 ; j<_Nc; j++)
     {
       _L1(i,j) = _dt * (_Lambda(i,j)+_Lambda(i,j-1))/(2*_hy*_hy*_A(i,j));
       _L2(i,j-1) = _dt * (_Lambda(i,j-1)+_Lambda(i,j))/(2*_hy*_hy*_A(i,j-1));
     }
   }
-  for (int i=1; i<_Ny; i++)
+  for (int i=1; i<_Nl; i++)
   {
-    for (int j=0; j<_Nx ; j++)
+    for (int j=0; j<_Nc ; j++)
     {
       _L3(i,j) = _dt * (_Lambda(i,j)+_Lambda(i-1,j))/(2*_hx*_hx*_A(i,j));
       _L4(i-1,j) = _dt * (_Lambda(i-1,j)+_Lambda(i,j))/(2*_hx*_hx*_A(i-1,j));
@@ -114,46 +116,40 @@ void Matrices::L1234()
 
 void Matrices::R()
 {
-  for (int i=0; i<_Ny; i++)
+  for (int i=0; i<_Nl; i++)
   {
-    for (int j=0; j<_Nx; j++)
+    for (int j=0; j<_Nc; j++)
     {
       R(i,j) = AStar(i,j) / A(i,j);
     }
   }
 }
 
-void Matrices::PP14()
+void Matrices::M()
 {
-  //Définition de P4, y=0, x varie
-  vector<Triplet<double>> triplets4;
-  triplets4.push_back({0,0,_R(0,0)-(_L2(0,0)+_L4(0,0))});
-  triplets4.push_back({1,0,_L2(0,1)});
-  //Définition de P1, y= y_max, x varie
-  vector<Triplet<double>> triplets1;
-  triplets3.push_back({0, 0,_R(0,_Ny-1)-(2*_L1(0,_Ny-1)+ _L2(0,_Ny-1))});
-  triplets3.push_back({1, 0,_L1(1,_Ny-1)});
+  vector<Triplet<double>> triplets;
 
-  for(int i=1 ; i<_Nx-1; i++)
+  for (int i=0 ; i<_Nl; i++)
   {
-    triplets4.push_back({i,i,_R(i,0)-(_L1(i,0)+_L2(i,0)+_L1(i,0))});
-    triplets4.push_back({i-1,i,_L2(i-1,0)});
-    triplets4.push_back({i+1,i,_L2(i+1,0)});
-
-    triplets1.push_back({i,i,_R(i,_Ny-1)-(_L1(i,_Ny-1)+_L2(i,_Ny-1)+_L1(i,_Ny-1))});
-    triplets1.push_back({i+1,i,_L1(i+1,_Ny-1)}});
-    triplets1.push_back({i-1,i,_L1(i-1,_Ny-1)}});
+    for (int j=0 ; j<_Nc;j++)
+    {
+      triplets.push_back({i*_Nc+j,i*_Nc+j,R(i,j)-(_L1(i,j)+_L2(i,j)+_L3(i,j)+_L4(i,j))})
+      if (i*_Nc+j>1)
+      {
+        triplets.push_back({i*_Nc+j-1,i*_Nc+j,_L1(i,j)})
+      }
+      else if(i*_Nc+j+1<(_Nl-1)*(_Nc-1))
+      {
+        triplets.push_back({i*_Nc+j+1,i*_Nc+j,_L2(i,j)})
+      }
+    }
   }
 
-  triplets1.push_back({_Nx-1,_Nx-1,_R(_Nx-1,_Ny-1)-(2*_L1(_Nx-1,_Ny-1)+ _L3(_Nx-1,_Ny-1))});
-  triplets1.push_back({_Nx-2,_Nx-1, _L1(_Nx-2,_Ny-1)});
-  triplets4.push_back({_Nx-1,_Nx-1, _R(_Nx-1,0)-(2*_L2(_Nx-1,0)+ _L3(_Nx-1,0)) });
-  triplets4.push_back({_Nx-2,_Nx-1, _L2(_Nx-2,0)});
-
-  P1.setFromTriplets(triplets1.begin(),triplets1.end());
-  P4.setFromTriplets(triplets4.begin(),triplets4.end());
 
 
+
+
+  }
 }
 
 void Matrices::M()
