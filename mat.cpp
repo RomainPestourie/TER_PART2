d3080bd699f069ecc7c217f324b89f75194231de
Romain <romain.pestourie24@gmail.com>
2019-03-27 11:13:08 +0100

    main.cc

--- mat.cpp
+++ mat.cpp
@@ -9,17 +9,18 @@
 #include "Dense"
 #include "Sparse"
 #include <chrono>
-#include "pyro.h"
+#include "mat.h"
 //#include <Eigen/SparseCholesky>
 
 using namespace Eigen;
 
 using namespace std;
 
-Matrices::Matrices( const double x_min, const double x_max, const double y_min, const double y_max):
-_Nx(Nl),_Ny(Nc),_x_min(x_min), _x_max(x_max), _y_min(y_min), _y_max(y_max)
+Matrices::Matrices( const double x_min, const double x_max, const double y_min, const double y_max, const int Nx , const int Ny):
+_Nl(Nx),_Nc(Ny),_x_min(x_min), _x_max(x_max), _y_min(y_min), _y_max(y_max)
 {
   // Paramètres
+  _Ar = 1;
   _M.resize(_Nl*_Nc,_Nl*_Nc);
   _Lm = 10;
   _RhoV = 1500;
@@ -32,16 +33,22 @@ _Nx(Nl),_Ny(Nc),_x_min(x_min), _x_max(x_max), _y_min(y_min), _y_max(y_max)
   _A.resize(_Nl,_Nc);
   _AStar.resize(_Nl,_Nc);
   _T.resize(_Nl,_Nc);
+  _Tvec.resize(_Nl*_Nc);
+  _Sol.resize(_Nl*_Nc);
   _LambV = 3;
   _LambP = 4;
   _Cpp = 1000;
   _Cpv = 6;
   _T0 = 293;
+  _TA = 500;
   _dt = 20 ;
   _t = 0;
+  _T.setZero();
+  _Sol.setZero();
+  _Rho.setZero();
+  _Lambda.setZero();
   _L1.resize(_Nl,_Nc); _L2.resize(_Nl,_Nc); _L3.resize(_Nl,_Nc); _L4.resize(_Nl,_Nc);
   //_P1.resize(_Nc,_Nc); _P2.resize(_Nc,_Nc); _P3.resize(_Nc,_Nc); _P4.resize(_Nc,_Nc);
-  _P.resize(_Nc*_Nl,_Nc*_Nl);
 
   for (int i=0 ; i<_Nl; i++)
   {
@@ -49,12 +56,27 @@ _Nx(Nl),_Ny(Nc),_x_min(x_min), _x_max(x_max), _y_min(y_min), _y_max(y_max)
     for (int j=0 ; j<_Nc ; j++)
     {
       cout << "j = " << j << endl;
-      _T(i,j) = _T0;
-      _Sol(j+_Ny*i) = _T(i,j);
-      _Rho(i,j) = _RhoV;
-      _Lambda(i,j) = 1;
-      _A(i,j) = Cpv;
-      _AStar(i,j) = Cpv;
+      _T.coeffRef(i,j) = _T0;
+      _Sol.coeffRef(j+_Nc*i) = _T.coeffRef(i,j);
+      _Rho.coeffRef(i,j) = _RhoV;
+      _Lambda.coeffRef(i,j) = 1;
+      _A.coeffRef(i,j) = _Cpv;
+      _AStar.coeffRef(i,j) = _Cpv;
+      _Tvec.coeffRef(j+i*_Nc) = _T.coeffRef(i,j);
+    }
+  }
+}
+
+void Matrices::Rho(double t)
+{
+  double C;
+  for (int i=0; i<_Nl; i++)
+  {
+    for (int j=0; j<_Nc ; j++)
+    {
+      C = _RhoV * _Ar * exp(-_TA/_T.coeffRef(i,j)) / (_RhoV-_RhoP);
+      _Rho.coeffRef(i,j) = (_RhoV-_RhoP) * exp(-C*t) + _RhoP;
+
     }
   }
 }
@@ -65,7 +87,7 @@ void Matrices::Xi()
   {
     for (int j=0 ; j<_Nc; j++)
     {
-      _Xi(i,j) = (_RhoV - _Rho(i,j)) / (_RhoV-_RhoP);
+      _Xi.coeffRef(i,j) = (_RhoV - _Rho.coeffRef(i,j)) / (_RhoV-_RhoP);
     }
   }
 }
@@ -76,7 +98,7 @@ void Matrices::Lambda()
   {
     for (int j=0 ; j<_Nc ; j++)
     {
-      _Lambda(i,j) = (1- _Xi(i,j)) * _LambV + _Xi(i,j) * _LambP ;
+      _Lambda.coeffRef(i,j) = (1- _Xi.coeffRef(i,j)) * _LambV + _Xi.coeffRef(i,j) * _LambP ;
     }
   }
 }
@@ -86,9 +108,8 @@ void Matrices::A()
   for (int i=0 ; i<_Nl; i++)
   {
     for (int j=0 ; j<_Nc ; j++)
-    {triplets.push_back({0,0,_R(0,0)-(L1(0,0)+_L2(0,0)+L3(0,0)+_L4(0,0))});
-  triplets.push_back({1,0,_L2(0,1)});
-      _A(i,j) = (1 - _Xi(i,j)) * _Cpv * _RhoV + _Xi(i,j) * _Cpp * _RhoP;
+    {
+      _A.coeffRef(i,j) = (1 - _Xi(i,j)) * _Cpv * _RhoV + _Xi(i,j) * _Cpp * _RhoP;
     }
   }
 }
@@ -120,7 +141,7 @@ void Matrices::R()
   {
     for (int j=0; j<_Nc; j++)
     {
-      R(i,j) = AStar(i,j) / A(i,j);
+      _R(i,j) = _AStar(i,j) / _A(i,j);
     }
   }
 }
@@ -133,26 +154,23 @@ void Matrices::M()
   {
     for (int j=0 ; j<_Nc;j++)
     {
-      triplets.push_back({i*_Nc+j,i*_Nc+j,R(i,j)-(_L1(i,j)+_L2(i,j)+_L3(i,j)+_L4(i,j))})
+      triplets.push_back({i*_Nc+j,i*_Nc+j,_R(i,j)-(_L1(i,j)+_L2(i,j)+_L3(i,j)+_L4(i,j))});
       if (i*_Nc+j>1)
       {
-        triplets.push_back({i*_Nc+j-1,i*_Nc+j,_L1(i,j)})
+        triplets.push_back({i*_Nc+j-1,i*_Nc+j,_L1(i,j)});
       }
       else if(i*_Nc+j+1<(_Nl-1)*(_Nc-1))
       {
-        triplets.push_back({i*_Nc+j+1,i*_Nc+j,_L2(i,j)})
+        triplets.push_back({i*_Nc+j+1,i*_Nc+j,_L2(i,j)});
+      }
+      else if ((i+1)*_Nc+j<(_Nl-1)*(_Nc-1))
+      {
+        triplets.push_back({i*_Nc+j,i*_Nc+j+_Nc,_L3(i,j)});
+      }
+      else if ((i-1)*_Nc+j>0)
+      {
+        triplets.push_back({i*_Nc+j,i*_Nc+j-_Nc,_L4(i,j)});
       }
     }
   }
-
-
-
-
-
-  }
-}
-
-void Matrices::M()
-{
-
 }
--- mat.h
+++ mat.h
@@ -16,35 +16,38 @@ private:
   const double _x_max;
   const double _y_min;
   const double _y_max;
-  const int _Nx;
-  const int _Ny;
-  const double _RhoV,_RhoP,_LambV,_LambP;
-  const double _Cpp , Cpv, _Lm;
-  double _L1 , _L2 , _L3 , _L4;
+  const int _Nl;
+  const int _Nc;
+  double _RhoV,_RhoP,_LambV,_LambP;
+  double _Cpp , _Cpv, _Lm, _Ar;
   double _t, _dt;
   double _hx,_hy;
-  double _T0;
+  double _T0,_TA;
 
-  Eigen::SparseMatrix<double> _M;
+  Eigen::SparseMatrix<double> _M,_T;
   Eigen::VectorXd _x;
   Eigen::VectorXd _y;
   Eigen::VectorXd _f;
   Eigen::VectorXd _Sol;
-  Eigen::MatrixXd _L1 , _L2 , _L3 , _L4, , _R;
-  Eigen::MatrixXd _T , _A, _AStar , _Lambda , _RhoStar , _Xi , _XiStar;
-  Eigen::SparseMatrix<double> _P, _P1 , _P4 ;
-  Eigen::VectorXd _AVec , _LambdaVec , _RhoVec , _XiVec ;
+  Eigen::MatrixXd _L1 , _L2 , _L3 , _L4 , _R;
+  Eigen::MatrixXd _Rho , _A, _AStar , _Lambda , _RhoStar , _Xi , _XiStar;
+  Eigen::VectorXd _Tvec ;
   Eigen::VectorXd _FS , _FN , _FE , _FO;
 
 public:
 Matrices(const double x_min, const double x_max, const double y_min, const double y_max,const int Nx, const int Ny);
 
+void Rho(double t);
 void Xi();
 void Lambda();
 void A();
 void M();
 void L1234();
 void R();
-}
-
+Eigen::VectorXd GetT() {return _T;};
+Eigen::SparseMatrix<double> GetM() {return _M;};
+Eigen::MatrixXd GetRho() {return _Rho;};
+Eigen::MatrixXd GetA() {return _A;};
+Eigen::MatrixXd GetXi() {return _Xi;};
+Eigen::MatrixXd GetLambda() {return _Lambda;};
 };
